0f37a2ae0b8d84d01495679bf31f5ea57b4c45b4
GaGo Aharonyan <gago9639@gmail.com>
2020-07-01 13:21:48 +0400

    second commit

---
+++ docs/precache-manifest.ce79f25e5586eff657a4d5a906471185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6828e75c574e8d57d954650f7dfd5bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362095dd5c175c113573",
+    "url": "/static/css/main.62f4f0d3.chunk.css"
+  },
+  {
+    "revision": "a1bbdae860f2eed210ad",
+    "url": "/static/js/2.878839b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.878839b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362095dd5c175c113573",
+    "url": "/static/js/main.9d0417aa.chunk.js"
+  },
+  {
+    "revision": "638826ac52a3c2e7ff3a",
+    "url": "/static/js/runtime-main.c27b1547.js"
+  }
+]);
